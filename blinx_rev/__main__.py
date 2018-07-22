5bf7341ca371e08acce3169a36fd35389b1309ef
Sam Kochanski <samkochanski@gmail.com>
2018-07-22 16:10:51 -0400

    fix porting to package

---
+++ blinx_rev/__main__.py
@@ -0,0 +1,43 @@
+from extract import Extractor
+from address import section_addresses
+import argparse
+
+
+def main() :
+    #TODO: handle invalid arguments, update vert/tri mode
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-me', '--mediapath', help='Path to media directory', type=str)
+    parser.add_argument('-m', '--mode', help='Mode of operation - chunk=0, vert/tri=1', type=int)
+    parser.add_argument('-s', '--section', help='Path to section file', type=str)
+    parser.add_argument('-so', '--soffset', help='Stringlist file entry offset', type=lambda x: int(x,0))
+    parser.add_argument('-co', '--coffset', help='Chunk file entry offset', type=lambda x: int(x,0))
+    parser.add_argument('-o', '--obj', help='Path to output obj file (vert/tri)', type=str)
+    parser.add_argument('-b', '--bin', help='Path to binary directory (vert/tri)', type=str)
+
+    parser.add_argument('-c', '--chunk', help='(DEPRICATED) Path to chunk file', type=str)
+    parser.add_argument('-p', '--pointers', help='(DEPRICATED) Chunk file contains pointers to next chunk', type=bool)
+    parser.add_argument('-vo', '--voffset', help='(DEPRICATED) Vertex offset in chunk file (in hex)', type=int)
+    parser.add_argument('-to', '--toffset', help='(DEPRICATED) Triangle offset in chunk file (in hex)', type=int)
+    parser.add_argument('-v', '--vert', help='(DEPRICATED) Path to vertex list file', type=str)
+    parser.add_argument('-t', '--tri', help='(DEPRICATED) Path to triangle list file', type=str)
+    args = parser.parse_args()
+
+    sections = section_addresses()
+
+
+    extract = Extractor(section_path=args.section, media_path=args.mediapath, vert_path=args.vert, tri_path=args.tri, obj_path=args.obj, bin_dir=args.bin)
+
+    sl = extract.parse_stringlist(start_off=args.soffset, section=sections.get('data'))
+
+    chunk = extract.read_chunk(usage='vt', section=sections.get('data'), start_off=args.coffset)
+    
+    extract.create_texture_coordinates(triset=chunk[1], stringlist=sl)
+
+
+
+    extract.write_verts(chunk[0])
+    extract.write_vt(extract.texpart_list)
+    extract.write_triparts_texture(chunk[1])
+
+if __name__ == '__main__' :
+    main()
--- blinx_rev/address.py
+++ blinx_rev/address.py
@@ -2,19 +2,28 @@ import csv
 
 #TODO: make a class to avoid redundant computation. Therefore, csv parsing only have to happen once.
 
+def section_addresses() :
+    addresses = {}
+    with open('../data/sectionaddress.csv', 'r') as csv_file :
+        sectionreader = csv.reader(csv_file)
+        next(sectionreader)
+        for line in sectionreader :
+            addresses[line[0]] = (int(line[1], 16), int(line[2], 16))
+    return addresses
+
 def rawaddress(virtaddress, section) :
     '''
     Converts a virtual address to a raw address using Blinx's defined section offsets.
     '''
     rawaddress = -1
-    with open('sectionaddress.csv', 'r') as csv_file :
+    with open('../data/sectionaddress.csv', 'r') as csv_file :
         section_index = __section_index(section)
         assert(section_index is not None)
         
         sectionreader = csv.reader(csv_file)
         line = next((x for i, x in enumerate(sectionreader) if i == section_index), None)
-        virtbase = int(line[1], 0)
-        rawbase = int(line[2], 0)
+        virtbase = int(line[1], 16)
+        rawbase = int(line[2], 16)
 
         offset = virtaddress - virtbase
         rawaddress = rawbase + offset
@@ -28,29 +37,22 @@ def find_section(virtaddress) :
     '''
     Finds the section containg a virtual address.
     '''
-    with open('sectionaddress.csv', 'r') as csv_file :
+    with open('../data/sectionaddress.csv', 'r') as csv_file :
         reader = csv.reader(csv_file)
         line = next()
-
+        #TODO: finish
 
 
 def __section_index(section) :
     candidate = section.upper()
 
     switcher = {}
-    with open('sectionaddress.csv', 'r') as csv_file :
+    with open('../data/sectionaddress.csv', 'r') as csv_file :
         
         reader = csv.reader(csv_file)
-        next(reader)
+        next(reader)            #skip header
         for c, line in enumerate(reader, 1) :
             switcher[line[0]] = c
 
     return switcher.get(candidate)
     
-
-
-def main() :
-    print(hex(rawaddress(0x1D8AC94, 'MAP11')))
-
-if __name__ == '__main__' :
-    main()
